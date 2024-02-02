7d05abea9c2a31b305022f73a31b81d4ac53f066
kennyowj961127 <kennyowj961127@gmail.com>
2024-02-02 17:19:16 +0800

    Added section 8 - folder

---
+++ section8/v2-spring-cloud-config/cards/src/main/java/com/kenny/cards/repository/CardsRepository.java
@@ -0,0 +1,12 @@
+package com.kenny.cards.repository;
+
+import com.kenny.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+}
