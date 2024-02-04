8a102a2e1740a0683fe7a80b07f9b565de5cb2e6
kennyowj961127 <kennyowj961127@gmail.com>
2024-02-04 18:11:25 +0800

    Added Section 9 - Gateway server

---
+++ section9/v2-spring-cloud-config/accounts/src/main/java/com/kenny/accounts/controller/CustomerController.java
@@ -0,0 +1,61 @@
+package com.kenny.accounts.controller;
+
+import com.kenny.accounts.dto.CustomerDetailsDto;
+import com.kenny.accounts.dto.ErrorResponseDto;
+import com.kenny.accounts.service.ICustomersService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(
+        name = "CRUD REST APIs for Customers",
+        description = "Controller class that handles CRUD REST APIs for Customers"
+)
+@RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@Validated
+public class CustomerController {
+
+    private final ICustomersService iCustomersService;
+
+    public CustomerController(ICustomersService iCustomersService) {
+        this.iCustomersService = iCustomersService;
+    }
+    @Operation(
+            summary = "Fetch Customer Details REST API",
+            description = "REST API to fetch Customer details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/fetchCustomerDetails")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestParam
+                                                                   @Pattern(regexp = "^[0-9]{8}$", message = "Mobile number must be 8 digits")
+                                                                   String mobileNumber) {
+        CustomerDetailsDto customerDetailsDto = iCustomersService.fetchCustomerDetails(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDetailsDto);
+    }
+}
---
+++ section9/v2-spring-cloud-config/accounts/src/main/java/com/kenny/accounts/service/IAccountsService.java
@@ -0,0 +1,32 @@
+package com.kenny.accounts.service;
+
+import com.kenny.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+    /**
+     * This method is used to create account
+     * @param customerDto
+     */
+    void createAccount(CustomerDto customerDto);
+
+    /**
+     * This method is used to fetch account details
+     * @param mobileNumber
+     * @return
+     */
+    CustomerDto fetchAccountDetails(String mobileNumber);
+
+    /**
+     * This method is used to update account
+     * @param customerDto
+     * @return
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     * This method is used to delete account
+     * @param customerDto
+     * @return
+     */
+    boolean deleteAccount(String mobileNumber);
+}
