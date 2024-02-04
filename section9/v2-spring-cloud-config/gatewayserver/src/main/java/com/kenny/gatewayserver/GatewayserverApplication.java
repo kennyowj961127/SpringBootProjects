cbc252e7b663acc7e1a247b6b5f814d601d57289
kennyowj961127 <kennyowj961127@gmail.com>
2024-02-04 19:16:55 +0800

    Added Section 9 - Implementing the custom routing using Spring Boot Cloud

--- section9/v2-spring-cloud-config/gatewayserver/src/main/java/com/kenny/gatewayserver/GatewayserverApplication.java
+++ section9/v2-spring-cloud-config/gatewayserver/src/main/java/com/kenny/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,11 @@ package com.kenny.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +15,26 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator kennyBankRouteConfig(RouteLocatorBuilder builder){
+		// Create a route locator that routes requests to the appropriate microservice
+		// All the path must be lowercase
+		return builder.routes()
+				.route(p -> p
+						.path("/kennybank/accounts/**")
+						.filters(f -> f.rewritePath("/kennybank/accounts/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/kennybank/loans/**")
+						.filters(f -> f.rewritePath("/kennybank/loans/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/kennybank/cards/**")
+						.filters(f -> f.rewritePath("/kennybank/cards/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CARDS"))
+				.build();
+	}
 }
