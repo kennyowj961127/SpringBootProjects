8fce0808375a050729380c41f0efdac23a2de23f
kennyowj961127 <kennyowj961127@gmail.com>
2024-02-11 12:17:30 +0800

    Added section 10 - added rate limiter in accounts

--- section_10/v2-spring-cloud-config/accounts/src/main/java/com/kenny/accounts/controller/AccountsController.java
+++ section_10/v2-spring-cloud-config/accounts/src/main/java/com/kenny/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.kenny.accounts.dto.CustomerDto;
 import com.kenny.accounts.dto.ErrorResponseDto;
 import com.kenny.accounts.dto.ResponseDto;
 import com.kenny.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -226,11 +227,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion(){
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
