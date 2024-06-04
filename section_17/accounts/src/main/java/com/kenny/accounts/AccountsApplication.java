265ef0f4764e6a523055367bb7d2838c4d6cd4e7
kennyowj961127 <123232019+kennyowj961127@users.noreply.github.com>
2024-06-04 20:14:38 +0800

    Update account service's discovery server
    Eureka server to Kubernetes Load Balancing

--- section_17/accounts/src/main/java/com/kenny/accounts/AccountsApplication.java
+++ section_17/accounts/src/main/java/com/kenny/accounts/AccountsApplication.java
@@ -8,10 +8,12 @@ import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 /*
     @ComponentScan({@ComponentScan("com.kenny.accounts.controller"),
                     @ComponentScan("com.kenny.accounts.service"),
